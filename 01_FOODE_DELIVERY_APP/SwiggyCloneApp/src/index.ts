38900c7141f207af1b2e3771e231a5ebe1ca1e13
Arundas MK <mkarundas@gmail.com>
2024-09-06 01:07:35 +0530

    env files created

--- 01_FOODE_DELIVERY_APP/SwiggyCloneApp/src/index.ts
+++ 01_FOODE_DELIVERY_APP/SwiggyCloneApp/src/index.ts
@@ -5,3 +5,12 @@ let app: express.Application = express();
 app.listen(3000, ()=> {
     console.log("Server is running");
 });
+
+app.use((req, res, next) => {
+    console.log('middleare')
+    next();
+});
+
+app.get('/api/user/login', (req, res)=> {
+    res.status(200).send("hello");
+});
